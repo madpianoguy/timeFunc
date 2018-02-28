e62c7c6d83f13608907a2c4cf07db9b0f41ce500
madpianoguy <joshuahinchliffe97@gmail.com>
2018-02-28 18:48:05 +0000

    Latest version

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from distutils.core import setup
 
 setup(
     name = 'timeFunc',
-    packages = ['timeFunc'],
+    scripts = ['timeFunc']
+    #packages = ['timeFunc'],
     version = '1.0',
     description = 'A decorator type timing function',
     author = 'Joshua Hinchliffe',
@@ -10,5 +11,5 @@ setup(
     url = 'https://github.com/madpianoguy/timeFunc',
     download_url = 'https://github.com/madpianoguy/timeFunc/archive/1.0.tar.gz',
     keywords = ['timing','decorator','madpianoguy'],
-    classifies = [],
+    classifiers = [],
 )
